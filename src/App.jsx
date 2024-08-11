b7f346891bf9252b01f10941a53080a0d4b6c5c4
Remi <91386739+alexithymiaq@users.noreply.github.com>
2024-08-11 17:09:15 -0400

    rest of files

---
+++ src/App.jsx
@@ -0,0 +1,103 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react'
+
+const API_KEY = "sk--Ktk701xhpdjjrCQlF4Hjo8Aei4cv7HpukbHONdPS9T3BlbkFJPcvD9y7YsKC9OOzA41llDQaCwVRqqkR7nCnBnnmekA";
+
+function App() {
+  const [typing, setTyping] = useState(false);
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello! I am UsiBot, your personal AI assistant! Feel free to ask me any questions!",
+      sender: "UsiBot",
+      direction: "incoming"
+    }
+  ]) // []
+
+  const handleSend = async (message) => {
+    const newMessage = {
+      message: message,
+      sender: "user",
+      direction: "outgoing"
+    }
+
+    const newMessages = [...messages, newMessage];
+
+    setMessages(newMessages);
+
+    setTyping(true);
+    
+    await processMessageToUsiBot(newMessages);
+  }
+
+  async function processMessageToUsiBot(chatMessages) {
+    let apiMessages = chatMessages.map((messageObject) => {
+      let role = "";
+      if(messageObject.sender === "UsiBot") {
+        role = "assistant"
+      } else {
+        role = "user"
+      }
+      return { role: role, content: messageObject.message }
+    });
+
+    const systemMessage = {
+      role: "system",
+      content: "You are a personal assistant, talk formally and do your best to help."
+    }
+
+    const apiRequestBody = {
+      "model": "gpt-3.5-turbo",
+      "messages": [
+        systemMessage,
+        ...apiMessages
+      ]
+    }
+
+    await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Authorization": "Bearer " + API_KEY,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(apiRequestBody)
+    }).then((data) => {
+      return data.json();
+    }).then((data) => {
+      console.log(data);
+      console.log(data.choices[0].message.content);
+      setMessages(
+        [...chatMessages, {
+          message: data.choices[0].message.content,
+          sender: "UsiBot",
+          direction: "incoming"
+        }]
+      );
+      setTyping(false);
+    });
+  }
+
+  return (
+    <div className='App'>
+      <div style={{ position: "relative", height: "600px", width: "700px"}}>
+        <MainContainer>
+          <ChatContainer>
+            <MessageList
+              scrollBehavior='smooth'
+              typingIndicator={typing ? <TypingIndicator content="UsiBot is typing" /> : null}
+            >
+              {messages.map((message, i) => {
+                return <Message key={i} model={message} />
+              })}
+            </MessageList>
+            <MessageInput placeholder='Type Message Here' onSend={handleSend}/>
+          </ChatContainer>
+        </MainContainer>
+      </div>
+    </div>
+  )
+}
+
+export default App
